42910df1f2bec050a732a20ab9063d23de571a06
Berkod-AI <berkodk.ai@gmail.com>
2023-11-25 15:44:52 +0100

    new dashboard layout implemented

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,18 +1,28 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  useTheme,
+  Avatar,
+} from "@mui/material";
 import { tokens } from "../../theme";
 import { mockDataElectricityPrices } from "../../data/mockDataElectricityPrices";
+import { mockDataDrivers} from "../../data/mockDataDrivers";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 //import EmailIcon from "@mui/icons-material/Email";
 //import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 //import PersonAddIcon from "@mui/icons-material/PersonAdd";
 //import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
-import LineChart from "../../components/LineChart";
-import GeographyChart from "../../components/GeographyChart";
+
 import BarChart from "../../components/BarChart";
 //import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 import { DatePicker, Card, SparkAreaChart, Text, Title } from "@tremor/react";
+import { List, ListItem } from "@mui/material";
+
+import WarningIcon from "@mui/icons-material/Warning";
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -27,8 +37,8 @@ const Dashboard = () => {
         <Box>
           <Button
             sx={{
-              backgroundColor: colors.blueAccent[700],
-              color: colors.grey[100],
+              backgroundColor: colors.blueAccent[900],
+              color: colors.primary[100],
               fontSize: "14px",
               fontWeight: "bold",
               padding: "10px 20px",
@@ -43,157 +53,24 @@ const Dashboard = () => {
       {/* GRID & CHARTS */}
       <Box
         display="grid"
-        gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows="140px"
-        gap="20px"
+        gridTemplateColumns="repeat(9, 1fr)"
+        gridAutoRows="min-content"
+        gap="12px"
       >
         {/* ROW 1 */}
+
         <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
+          gridColumn="span 2" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 1"
         >
-          <Box>
-            <DatePicker className="max-w-sm mx-auto" />;
-          </Box>
+          <DatePicker />
         </Box>
         <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        ></Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        ></Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
+          gridColumn="span 5" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 2"
         ></Box>
 
         {/* ROW 2 */}
-        <Box
-          gridColumn="span 8"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-        >
-          <Box
-            mt="25px"
-            p="0 30px"
-            display="flex "
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              <Typography
-                variant="h3"
-                fontWeight="bold"
-                color={colors.greenAccent[500]}
-              >
-                SOC 29/10/2023
-              </Typography>
-            </Box>
-            <Box>
-              <IconButton>
-                <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
-                />
-              </IconButton>
-            </Box>
-          </Box>
-          <Box height="250px" m="-20px 0 0 0">
-            <LineChart isDashboard={true} />
-          </Box>
-        </Box>
-        {/**/}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          overflow="auto"
-        >
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            borderBottom={`4px solid ${colors.primary[500]}`}
-            colors={colors.grey[100]}
-            p="15px"
-          >
-            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
-              Electricity price
-            </Typography>
-          </Box>
-          <Box padding={"43px"}>
-            <Card className="max-w-lg flex items-center justify-between mx-auto px-4 py-6.5">
-              <div className="flex items-center space-x-2.5">
-                <Title>Electricity Prices</Title>
-              </div>
-              <div><Text className="hidden sm:block">Hourly Rates</Text></div>
-              <SparkAreaChart
-                data={mockDataElectricityPrices.map((item) => ({
-                  time: item.time,
-                  Price: item.price,
-                }))}
-                categories={["Price"]}
-                index={"time"}
-                colors={["emerald"]}
-                className="h-20 w-36"
-              />
-              <div className="flex items-center space-x-2.5 p-6">
-                {/* You can add additional details here, like current price, change, etc. */}
-                <span className="font-medium rounded text-gray-700">
-                  Current Price
-                </span>
-                <span className="px-2 py-1 text-sm font-medium rounded text-white bg-emerald-500">
-                  +1.72%
-                </span>
-              </div>
-            </Card>
-          </Box>
-        </Box>
-
-        {/* ROW 3 */}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h3" fontWeight="600" textAlign="center">
-            Bus-007
-          </Typography>
-          <Typography variant="h3" fontWeight="600" textAlign="center">
-            SOC%
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            height="100%"
-          >
-            <ProgressCircle size="130" />
-            <Typography
-              variant="h3"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "10px" }}
-            >
-              91% SOC
-            </Typography>
-            <Typography variant="subtitle1">State of Charge</Typography>
-          </Box>
-        </Box>
         <Box
           gridColumn="span 4"
           gridRow="span 2"
@@ -204,29 +81,149 @@ const Dashboard = () => {
             fontWeight="600"
             sx={{ padding: "30px 30px 0 30px" }}
           >
-            Sales Quantity
+            FLEET SOC %
           </Typography>
           <Box height="250px" mt="-20px">
             <BarChart isDashboard={true} />
           </Box>
         </Box>
+
         <Box
-          gridColumn="span 4"
+          gridColumn="span 1" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 2"
+        ></Box>
+        {/* ALERT Box */}
+        <Box
+          gridColumn="span 3"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
-          padding="30px"
+          p={2}
         >
-          <Typography
-            variant="h5"
-            fontWeight="600"
-            sx={{ marginBottom: "15px" }}
-          >
-            Geography Based Traffic
-          </Typography>
-          <Box height="200px">
-            <GeographyChart isDashboard={true} />
+          <Box display="flex" alignItems="center" gap={1}>
+            <WarningIcon sx={{ color: "red" }} /> {/* Alert icon in red */}
+            <Typography variant="h5" fontWeight="600" sx={{ color: "red" }}>
+              ALERT
+            </Typography>
+          </Box>
+          {/* List of alerts */}
+          <Box>
+            <List>
+              <ListItem>BUS 1 - NEED CHARGING</ListItem>
+              <ListItem>BUS 3 - UNDER 20%</ListItem>
+              <ListItem>BUS 8 - CHARGING OVER 80%</ListItem>
+              <ListItem>BUS 11 - CHARGING OVER 80%</ListItem>
+              {/* More list items... */}
+            </List>
           </Box>
         </Box>
+        <Box
+          gridColumn="span 2" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 1"
+        ></Box>
+
+        {/**/}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 1"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box display="flex" flexDirection="column" p="15px">
+            <Box borderBottom={`4px solid ${colors.primary[500]}`}>
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+              >
+                Electricity Price Prediction
+              </Typography>
+              <Typography variant="subtitle1" color={colors.grey[300]}>
+                Hourly Forecast for Next 24 Hours
+              </Typography>
+            </Box>
+
+            <Box mt={3} mx="auto" style={{ maxWidth: "90%" }}>
+              <SparkAreaChart
+                data={mockDataElectricityPrices.map((item) => ({
+                  time: item.time,
+                  Price: item.price,
+                }))}
+                categories={["Price"]}
+                index={"time"}
+                colors={["emerald"]}
+                className="h-20 w-36"
+              />
+            </Box>
+
+            <Box mt={3} display="flex" justifyContent="center">
+              <Typography variant="subtitle1" color={colors.grey[700]}>
+                Current Price:{" "}
+                <span style={{ fontWeight: "bold" }}>€0.1823 per KWh</span>
+              </Typography>
+              <Typography variant="subtitle1" color="green" ml={2}>
+                Change: +0.32%
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+        {/** */}
+
+        {/* ROW 3 */}
+        
+        <Box
+          gridColumn="span 4" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 1"
+        ></Box>
+        
+
+        {/*ROW4 */}
+        <Box
+          gridColumn="span 2" // Adjust this span to control the width of the "Select date" component
+          gridRow="span 1"
+        ></Box>
+
+        <Box
+          gridColumn="span 5"
+          gridRow="span 3"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+             <Typography variant="h5" fontWeight="600">
+                  ACTIVE DRIVERS
+                </Typography>
+                <Box
+                  display="flex"
+                  justifyContent="space-around"
+                  alignItems="center"
+                  mt={2}
+                >
+                  {mockDataDrivers.map((driver, index) => (
+                    <Box key={index} textAlign="center">
+                      <Avatar
+                        src={driver.imageUrl}
+                        alt={driver.name}
+                        sx={{ width: 56, height: 56 }}
+                      />
+                      <Typography variant="subtitle2">{driver.name}</Typography>
+                    </Box>
+                  ))}
+                </Box>
+            <Box>
+              
+               
+              
+            </Box>
+          </Box>
+
+          {/*Row4 Geo */}
+        </Box>
+        
       </Box>
     </Box>
   );
