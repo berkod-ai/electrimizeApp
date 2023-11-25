3b84ce8940788dc463a0988e0ddfbd56eacf64ab
Berkod-AI <berkodk.ai@gmail.com>
2023-11-25 15:43:12 +0100

    mockdatadriver added

---
+++ src/data/mockDataDrivers.js
@@ -0,0 +1,8 @@
+export const mockDataDrivers = [
+    { name: 'Alice', imageUrl: 'https://via.placeholder.com/150' },
+    { name: 'Bob', imageUrl: 'https://via.placeholder.com/150' },
+    { name: 'Carlos', imageUrl: 'https://via.placeholder.com/150' },
+    { name: 'Diana', imageUrl: 'https://via.placeholder.com/150' },
+    { name: 'Eva', imageUrl: 'https://via.placeholder.com/150' },
+    { name: 'Frank', imageUrl: 'https://via.placeholder.com/150' }
+  ];
