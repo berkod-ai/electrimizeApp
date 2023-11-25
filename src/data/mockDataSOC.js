7642c6297e19bc57cd22dfb2dfca06b8baea7ee8
Berkod-AI <berkodk.ai@gmail.com>
2023-11-25 15:44:11 +0100

    mocdataSOC updated

---
+++ src/data/mockDataSOC.js
@@ -0,0 +1,108 @@
+export const mockDataSOC = [
+  {
+    id: 'Bus-001',
+    data: [
+      { time: "00:00", soc: 31 },
+      { time: "01:00", soc: 51 },
+      { time: "02:00", soc: 69 },
+      { time: "03:00", soc: 83 },
+      { time: "04:00", soc: 98 },
+      { time: "05:00", soc: 100 },
+      { time: "06:00", soc: 100 },
+      { time: "07:00", soc: 100 },
+      { time: "08:00", soc: 94 },
+      { time: "09:00", soc: 88 },
+      { time: "10:00", soc: 85 },
+      { time: "11:00", soc: 82 },
+      { time: "12:00", soc: 79 },
+      { time: "13:00", soc: 76 },
+      { time: "14:00", soc: 73 },
+      { time: "15:00", soc: 70 },
+      { time: "16:00", soc: 67 },
+      { time: "17:00", soc: 64 },
+      { time: "18:00", soc: 61 },
+      { time: "19:00", soc: 58 },
+      { time: "20:00", soc: 55 },
+      { time: "21:00", soc: 52 },
+      { time: "22:00", soc: 49 },
+      { time: "23:00", soc: 46 }
+    ]
+  },
+  {
+    id: 'Bus-002',
+    data: [
+      { time: '00:00', soc: 44 },
+      { time: '01:00', soc: 55 },
+      { time: '02:00', soc: 66 },
+      { time: '03:00', soc: 76 },
+      { time: '04:00', soc: 87 },
+      { time: '05:00', soc: 98 },
+      { time: '06:00', soc: 96 },
+      { time: '07:00', soc: 94 },
+      { time: '08:00', soc: 83 },
+      { time: '09:00', soc: 86 },
+      { time: '10:00', soc: 88 },
+      { time: '11:00', soc: 74 },
+      { time: '12:00', soc: 84 },
+      { time: '13:00', soc: 82 },
+      { time: '14:00', soc: 62 },
+      { time: '15:00', soc: 78 },
+      { time: '16:00', soc: 76 },
+      { time: '17:00', soc: 86 },
+      { time: '18:00', soc: 72 },
+      { time: '19:00', soc: 42 },
+      { time: '20:00', soc: 83 },
+      { time: '21:00', soc: 66 },
+      { time: '22:00', soc: 81 },
+      { time: '23:00', soc: 80 }
+    ]
+  },
+
+  {
+    id: 'Bus-003',
+    data: [
+      { time: '00:00', soc: 49 },
+      { time: '01:00', soc: 59 },
+      { time: '02:00', soc: 68 },
+      { time: '03:00', soc: 78 },
+      { time: '04:00', soc: 87 },
+      { time: '05:00', soc: 97 },
+      { time: '06:00', soc: 92 },
+      { time: '07:00', soc: 87 },
+      { time: '08:00', soc: 94 },
+      { time: '09:00', soc: 89 },
+      { time: '10:00', soc: 92 },
+      { time: '11:00', soc: 73 },
+      { time: '12:00', soc: 90 },
+      { time: '13:00', soc: 100 },
+      { time: '14:00', soc: 91 },
+      { time: '15:00', soc: 77 },
+      { time: '16:00', soc: 75 },
+      { time: '17:00', soc: 67 },
+      { time: '18:00', soc: 51 },
+      { time: '19:00', soc: 41 },
+      { time: '20:00', soc: 37 },
+      { time: '21:00', soc: 17 },
+      { time: '22:00', soc: 12 },
+      { time: '23:00', soc: 15 }
+    ]
+  }
+ 
+];
+
+
+export const simplifiedMockDataSOC = [
+  { bus: "Bus-001", soc: 30 }, // Last SOC value for Bus-001
+  { bus: "Bus-002", soc: 100 }, // Last SOC value for Bus-002
+  { bus: "Bus-003", soc: 15 }, // Last SOC value for Bus-003
+  { bus: "Bus-004", soc: 60 }, 
+  { bus: "Bus-005", soc: 75 }, 
+  { bus: "Bus-006", soc: 50 }, 
+  { bus: "Bus-007", soc: 35 }, 
+  { bus: "Bus-008", soc: 90 }, 
+  { bus: "Bus-009", soc: 55 }, 
+  { bus: "Bus-010", soc: 40 }, 
+  { bus: "Bus-011", soc: 65 }, 
+];
+
+
