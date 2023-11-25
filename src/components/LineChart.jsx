a231e2b36306a6280a46b500b829b76b5755eab1
Berkod-AI <berkodk.ai@gmail.com>
2023-11-25 15:43:50 +0100

    line chart deleted from dashboard

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,7 @@
 import { ResponsiveLine } from "@nivo/line";
 import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
-import { mockDataSOCLine as data } from "../data/mockDataSOCLine";
+import { mockDataSOC as data } from "../data/mockDataSOC";
 
 const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
   const theme = useTheme();
